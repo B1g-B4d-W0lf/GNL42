00d41ae28a1e3bc0e4642bd84dd2b7c5e49ed8e1
Big Bad Wolf <wfreulon@student.42.fr>
2022-12-04 17:37:57 +0100

    ntm

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wfreulon <wfreulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 23:04:16 by wfreulon          #+#    #+#             */
-/*   Updated: 2022/12/01 01:33:33 by wfreulon         ###   ########.fr       */
+/*   Updated: 2022/12/04 16:44:34 by wfreulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@
 # include <stdint.h>
 # include <fcntl.h>
 
+# ifndef BUFFER_SIZE
+
+#	define BUFFER_SIZE 40
+
+# endif
 typedef struct s_list
 {
 	char			*content;
